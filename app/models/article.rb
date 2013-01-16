cf516fd74d6604d24abe24e239e34b3547787a97
Albert Song <albb0920@gmail.com>
2013-01-17 02:17:18 +0800

    Add url column to articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  attr_accessible :content, :title, :reporter_ids, :reporters_attributes
+  attr_accessible :content, :title, :reporter_ids, :reporters_attributes, :url
   has_and_belongs_to_many :reporters
   accepts_nested_attributes_for :reporters, allow_destroy: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130116172453) do
+ActiveRecord::Schema.define(:version => 20130116181616) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "url"
   end
 
   create_table "articles_reporters", :id => false, :force => true do |t|
