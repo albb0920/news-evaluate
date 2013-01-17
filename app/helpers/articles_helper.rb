9b0c180f832b8beefe38780c82f727d14e365a08
Albert Song <albb0920@gmail.com>
2013-01-17 18:12:51 +0800

    Add color support.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,14 @@
 module ArticlesHelper
+  def score_class(article)
+    score = article.score
+    if score > 90
+      " gold"
+    elsif (70..90).include? score
+      " silver"
+    elsif (50..70).include? score
+      " orange"
+    else
+      " red"
+    end
+  end
 end
